05a02eba01e585c33942bd8b95eb84a3939478c8
edwingudfriend <edwingudfriend@gmail.com>
2025-04-24 16:23:07 +0300

    Modified unused import

--- src/main/java/com/example/temperaturemonitoringsystem/controller/ArrayController.java
+++ src/main/java/com/example/temperaturemonitoringsystem/controller/ArrayController.java
@@ -1,7 +1,7 @@
 package com.example.temperaturemonitoringsystem.controller;
 
 import com.example.temperaturemonitoringsystem.service.ArrayService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
